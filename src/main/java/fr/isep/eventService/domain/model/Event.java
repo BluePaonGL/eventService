761d85003083fc59f0a937a33233f77dd33f468d
MorganDelange <74045908+MorganDelange@users.noreply.github.com>
2022-05-12 14:08:12 +0200

    Add of event entity

--- src/main/java/fr/isep/eventService/domain/model/Event.java
+++ src/main/java/fr/isep/eventService/domain/model/Event.java
@@ -1,5 +1,33 @@
 package fr.isep.eventService.domain.model;
 
+import lombok.*;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="`event`")
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long eventId;
+
+    private String name;
+    private String starting_campus;
+    private String location;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @Temporal(TemporalType.TIME)
+    private Date starting_time;
+
+
+
 }
