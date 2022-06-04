0dedcc9cce7a1770082d6160d107de6d3f1fee76
Artcann <art.cann@orange.fr>
2022-06-04 19:27:12 +0200

    Ratio

--- src/main/java/fr/isep/eventService/infrastructure/adapter_repository_db/repository/EventParticipantRepository.java
+++ src/main/java/fr/isep/eventService/infrastructure/adapter_repository_db/repository/EventParticipantRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 public interface EventParticipantRepository extends JpaRepository<EventParticipantDAO, Long> {
     List<EventParticipantDAO> getEventParticipantDAOSByEventId(String eventId);
 
-    @Query("Select eventParticipant from EventParticipantDAO eventParticipant left join EventDAO event on eventParticipant.eventId=event.eventId order by event.date asc ")
     List<EventParticipantDAO> getEventParticipantDAOSByParticipantId(String participantId);
 
     EventParticipantDAO findByEventIdAndParticipantId(String eventId, String ParticipantId);
